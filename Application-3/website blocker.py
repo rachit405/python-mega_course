58ca14a77e88ce8d501b7b40eba934af12c9581b
rachit405 <51585173+rachit405@users.noreply.github.com>
2021-08-15 01:20:47 +0530

    Add files via upload

---
+++ Application-3/website blocker.py
@@ -0,0 +1,30 @@
+import time
+from datetime import datetime as dt
+
+
+#hosts_path = "hosts"
+hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
+redirect = "127.0.0.1"
+website_lists = ["www.facebook.com","facebook.com"]
+
+while True:
+	if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
+		print("working hours....")
+		with open(hosts_path, 'r+') as file:
+			content = file.read()
+			for website in website_lists:
+				if website in content:
+					pass
+				else:
+					 file.write(redirect+ " "+ website+"\n")	
+	else:
+		with open(hosts_path,'r+') as file:
+			content = file.readlines()
+			file.seek(0)
+			for line in content:
+				if not any(website in line for website in website_lists):
+					file.write(line)
+			file.truncate()		
+		print("fun hours....")
+	time.sleep(5)
+		
